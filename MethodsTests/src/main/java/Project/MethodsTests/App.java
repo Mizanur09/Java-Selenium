cbfae9d5384d8ae54d7ca28eb73cd14f28576ebf
mizanur09 <mizanur.rahman0918@gmail.com>
2018-11-28 23:49:21 -0500

    Started a Project with gitignore files

---
+++ MethodsTests/src/main/java/Project/MethodsTests/App.java
@@ -0,0 +1,13 @@
+package Project.MethodsTests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
